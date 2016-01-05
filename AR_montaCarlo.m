ac420cbc3612cc3ef9274bb791d4ced3e79ef499
Avi <avi.illouz@gmail.com>
2016-01-05 16:02:13 +0200

    part 2 monte carlo

--- AR_montaCarlo.m
+++ AR_montaCarlo.m
@@ -1,16 +1,14 @@
-samples = 1000000;
-n = 100000;
+warning off
+samples = 100000;
+n = 1000;
 delta = 0.001;
 
 g = genAR(zeros(samples,1),.5,n);
-xn = zeros(samples,1);
-for i=[1 2 3 10^4 10^5 10^6 ]
-   xn(:,end+1)= g(:,i); 
+v = [1 2 3 0.5*n 0.75*n n];
+for i=1:length(v)
+    subplot(2,3,i)
+    [y,x] = hist(g(:,v(i)),[-3:delta:3]);
+    y=y/(samples*delta);
+    bar(x,y);
 end
-
-for i = 2:7
-[x,y] = hist(xn(:,i),[3:delta:3]);
-y=y/(samples*delta);
-figure;
-bar(x,y)
-end
+    
