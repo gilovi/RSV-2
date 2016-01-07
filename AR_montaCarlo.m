4ae979ddfc5a5c291d658fc2c57829bb1713e1b5
gilovi <gilovi@users.noreply.github.com>
2016-01-07 09:57:03 +0200

    second rv added

--- AR_montaCarlo.m
+++ AR_montaCarlo.m
@@ -2,8 +2,9 @@ warning off
 samples = 100000;
 n = 1000;
 delta = 0.001;
-
-g = genAR(zeros(samples,1),.5,n);
+%%
+k = zeros(samples,1);
+g = genAR(k ,.5,n);
 v = [1 2 3 0.5*n 0.75*n n];
 for i=1:length(v)
     subplot(2,3,i)
@@ -11,4 +12,14 @@ for i=1:length(v)
     y=y/(samples*delta);
     bar(x,y);
 end
-    
+%%    
+figure;
+k = rand(samples,1) + 2;
+g2 = genAR(k ,.5,n);
+v = [1 2 3 0.5*n 0.75*n n];
+for i=1:length(v)
+    subplot(2,3,i)
+    [y,x] = hist(g2(:,v(i)),[-2:delta:2]);
+    y=y/(samples*delta);
+    bar(x,y);
+end
